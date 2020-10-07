7295c9e8074700df6ef2429956ad2796344df85c
Paul Klemm <paul.klemm@sf.mpg.de>
2020-10-07 10:14:29 +0200

    Add trapdiff export function

--- R/tools.R
+++ R/tools.R
@@ -39,3 +39,70 @@ get_test_counts_marta <- function() {
     # Round values to comply with DESeq2 requirements
     dplyr::mutate_if(is.numeric, round)
 }
+
+#' Run trapdiff and save report file
+#' 
+#' @export
+#' @import magrittr rmarkdown
+#' 
+#' @param counts Counts per sample as dataframe
+#' @param path_config_json Configuration file telling trapdiff group associations
+#' @param out_path Output folder for report and intermediate files
+#' @param biotypes_filter Filter genes for these biotypes
+#' @param counts_min A gene is included when at least one sample contains counts_min reads
+#' @param save_rds Save rds file of results
+#' @param save_excel Save Excel sheets of results
+#' @param save_figures Save output figures as png and pdf in out_path/figures
+#' @param ensembl_version Ensembl version used for attaching biomart variables
+#' @param clean_output Clean intermediate rmarkdown render files
+trapdiff <- function(
+  counts,
+  path_config_json,
+  out_path,
+  ensembl_version = 100,
+  biotypes_filter = "protein_coding",
+  counts_min = 30,
+  save_rds = TRUE,
+  save_excel = TRUE,
+  save_figures = TRUE
+) {
+  # Be sure the output path exists
+  if (!dir.exists(out_path)) {
+    paste0(
+      "The output path ",
+      out_path,
+      " does not exist. I will create it now."
+    ) %>%
+    message()
+    dir.create(
+      path = out_path,
+      showWarnings = TRUE,
+      recursive = TRUE
+    )
+  }
+
+  # Render command with all parameters
+  rmarkdown::render(
+    system.file("rmd/trapdiff/trapdiff.Rmd", package = "trapdiff"),
+    params = list(
+      path_out = out_path,
+      path_config_json = path_config_json,
+      biotypes_filter = biotypes_filter,
+      counts_min = counts_min,
+      save_rds = save_rds,
+      save_excel = save_excel,
+      save_figures = save_figures,
+      ensembl_version = ensembl_version,
+      counts = counts
+    ),
+    # Change the intermediate path to the output to avoid write access errors
+    intermediates_dir = out_path,
+    knit_root_dir = out_path,
+    # Clean intermediate files created during rendering.
+    clean = TRUE,
+    output_dir = out_path,
+    output_options = list(
+      self_contained = TRUE
+    )
+  )
+}
