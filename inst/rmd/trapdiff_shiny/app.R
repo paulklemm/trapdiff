82f6d0e4ff01163f797e5eceee3a33a61133af03
Paul Klemm <paul.klemm@sf.mpg.de>
2021-04-15 15:19:35 +0200

    Make shiny plots prettier

--- inst/rmd/trapdiff_shiny/app.R
+++ inst/rmd/trapdiff_shiny/app.R
@@ -8,6 +8,23 @@ options(
   rmyknife.biomart_mirror_host = "useast.ensembl.org"
 )
 library(rmyknife)
+# Define ggplot2 options
+ggplot2::theme_set(
+  ggplot2::theme_minimal(
+    base_size = 12
+  )
+)
+ggplot2::theme_update(
+  axis.ticks = ggplot2::element_line(color = "grey92"),
+  axis.ticks.length = ggplot2::unit(.5, "lines"),
+  panel.grid.minor = ggplot2::element_blank(),
+  legend.title = ggplot2::element_text(size = 12),
+  legend.text = ggplot2::element_text(color = "grey30"),
+  plot.title = ggplot2::element_text(size = 18, face = "bold"),
+  plot.subtitle = ggplot2::element_text(size = 12, color = "grey30"),
+  plot.caption = ggplot2::element_text(size = 9, margin = ggplot2::margin(t = 15)),
+  plot.title.position = 'plot'
+)
 
 fc_scatterplot <- function(
   ggplot_dat,
